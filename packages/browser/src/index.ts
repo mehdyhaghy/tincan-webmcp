bd55fe3234492cd90cc3a3938b2bef93548e8590
Mehdy Haghy <mehdy@vitasoft.com>
2026-08-29 12:29:33 -0500

    refactor(browser): remove duplicate core exports

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -2,6 +2,5 @@ export * from "./console-instrumentation";
 export * from "./recorder";
 export * from "./payload";
 export * from "./ring-buffer";
-export * from "./sanitize";
-export type * from "./types";
 export type * from "@tincan-webmcp/otel";
+export * from "@tincan-webmcp/core";
